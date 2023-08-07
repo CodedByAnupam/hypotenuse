6ff0e38d1addaaa06ee6e8b3064e8c454a2f045f
Anupam789 <anupamsachin@gmail.com>
2023-08-07 20:02:28 +0100

    first commit

---
+++ src/hypotenuse.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+from math import sqrt
+
+
+def hypotenuse(leg1, leg2):
+    return sqrt((leg1 * leg1) + (leg2 * leg2))
+
+
+if __name__ == "__main__":
+    hypotenuse()
